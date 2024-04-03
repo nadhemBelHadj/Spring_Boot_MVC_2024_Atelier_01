8257b5875d2de426815aca8f3bb00d9c421602bf
Nadhem Bel Hadj <39857478+nadhemBelHadj@users.noreply.github.com>
2024-04-03 15:58:06 +0100

    Update ProduitServiceImpl.java

--- src/main/java/com/nadhem/produits/service/ProduitServiceImpl.java
+++ src/main/java/com/nadhem/produits/service/ProduitServiceImpl.java
@@ -1,12 +1,12 @@
 package com.nadhem.produits.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import com.nadhem.produits.entities.Produit;
 import com.nadhem.produits.repos.ProduitRepository;
 
+@Service
 public class ProduitServiceImpl implements ProduitService{
 
 	@Autowired
